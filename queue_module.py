21a5b3d4d6fa6dae72f26703bab029d38c2fae8c
Kenta <kentarin1015@gmail.com>
2024-05-18 17:04:36 +1000

    Implemented __len__ and __str__ methods in CircularArrayQueue

--- queue_module.py
+++ queue_module.py
@@ -8,3 +8,11 @@ class CircularArrayQueue:
 
 def IsEmpty(self):
         return self.size == 0
+    
+def __len__(self):
+        return self.size
+
+def __str__(self):
+        if self.IsEmpty():
+            return "Queue is empty"
+        return str([self.queue[(self.front + i) % self.capacity] for i in range(self.size)])
