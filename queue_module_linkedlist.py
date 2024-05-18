6eba60982fecd75b682147b329fd6731fe53579a
Kenta <kentarin1015@gmail.com>
2024-05-18 20:44:06 +1000

    Implemented add() method in LinkedListQueue

--- queue_module_linkedlist.py
+++ queue_module_linkedlist.py
@@ -12,3 +12,11 @@ class LinkedListQueue:
         return self.size == 0
     def __len__(self):
         return self.size
+    def add(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.front = self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+        self.size += 1
